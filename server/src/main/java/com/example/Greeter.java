66472e6434bffcb64d19026dda83e0d24054386b
Sandeep Rajput <github@rajputsandeep.com>
2018-06-05 17:50:07 +0530

    edited Javadoc comment in Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,6 @@ public class Greeter {
     *@param "Sandeep Rajput"
     *@return "Engineer at einfochips"
     */
-
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
